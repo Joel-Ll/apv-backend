443a75b64ef79ac6d94e109c6ebe97c2e055c2d6
Joel-Ll <joellanos654@gmail.com>
2023-12-27 17:19:18 -0400

    first commit

---
+++ routes/veterinarioRoutes.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { autenticar, comprobarToken, confirmar, nuevoPassword, olvidePassword, perfil, registrar, actualizarPerfil, actualizarPassword } from '../controllers/veterinarioController.js';
+import checkAuth from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+// Area publica
+router.post('/', registrar )
+router.get('/confirmar/:token', confirmar );
+router.post('/login', autenticar );
+router.post('/olvide-password', olvidePassword );
+router.get('/olvide-password/:token', comprobarToken);
+router.post('/olvide-password/:token', nuevoPassword );
+
+// Area privada
+router.get('/perfil', checkAuth, perfil );
+router.put('/perfil/:id', checkAuth, actualizarPerfil );
+router.put('/actualizar-password', checkAuth, actualizarPassword );
+
+export default router;
+
