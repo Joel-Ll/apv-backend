c23c8a7b0a58144c8a25b4d0ff659ff9ebf70e5c
Joel-Ll <joellanos654@gmail.com>
2023-12-27 18:23:21 -0400

    fix

--- index.js
+++ index.js
@@ -12,18 +12,22 @@ dotenv.config();
 conectarDB();
 
 const dominiosPermitidos = [process.env.FRONTEND_URL];
-const corsOptions = {
-	origin: function(origin, callback) {
-		if( dominiosPermitidos.indexOf(origin) !== -1 ) {
-			// El origen del Request esta permitido
-			callback(null, true);
-		} else {
-			callback( new Error('No permitido por CORS'));
-		} 
-	}
-} 
+// const corsOptions = {
+// 	origin: function(origin, callback) {
+// 		if( dominiosPermitidos.indexOf(origin) !== -1 ) {
+// 			// El origen del Request esta permitido
+// 			callback(null, true);
+// 		} else {
+// 			callback( new Error('No permitido por CORS'));
+// 		} 
+// 	}
+// } 
 
-app.use( cors(corsOptions));
+app.use(
+  cors({
+    origin: '*',
+  }),
+);
 
 
 app.use('/api/veterinarios', veterinarioRoutes ); 
